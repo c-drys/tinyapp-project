8566c72d5ff3558803abba5c051f5655b1323cca
drystar <chris.drysdale12@gmail.com>
2019-10-30 19:22:01 +0000

    fix define long --> longURL

--- express_server.js
+++ express_server.js
@@ -6,14 +6,14 @@ app.set("view engine", "ejs");
 
 // convert the request body from a buffer to a readable string
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// implement to alwasy return 6 characters
+// implement to always return 6 characters
 function generateRandomString() {
   let str = '';
   let alphanum = 'abcdefghijklmnopqrstuvwxyz0123456789';
@@ -23,10 +23,6 @@ function generateRandomString() {
   }
   return str;
 }
-
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
 
 // add GET index_ejs template route
 app.get("/urls", (req, res) => {
@@ -50,23 +46,24 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);         
 });
 
-app.get('/urls:shortURL', (req, res) => {
+app.get('/urls/:shortURL', (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
-    long: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL]
   };
-  res.render('urls_show', templateVars)
+  res.render('urls_show', templateVars);
+});
 
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-//add POST edit route redirect ??
-app.post("/urls/:shortURL/edit", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
-});
+// //add POST edit route redirect ??
+// app.post("/urls/:shortURL/edit", (req, res) => {
+//   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+//   res.render("urls_show", templateVars);
+// });
 
 // add POST delete route re-direct
 app.post('/urls/:shortURL/delete', (req, res) => {
@@ -85,11 +82,14 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-// app.post("/urls", (req, res) => {
+  // app.post("/urls", (req, res) => {
   //   console.log(req.body);  // Log the POST request body to the console
   //   res.send("Ok");         // Respond with 'Ok' (we will replace this)
   // });
   
+  // app.get("/", (req, res) => {
+  //   res.send("Hello!");
+  // });
   
   // app.get("/hello", (req, res) => {
   //   res.send("<html><body>Hello <b>Chris</b></body></html>\n");
