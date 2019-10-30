8196aea0e918c582928417a42388169d3a1560d8
drystar <chris.drysdale12@gmail.com>
2019-10-30 07:11:04 +0000

    Set up EJS

--- express_server.js
+++ express_server.js
@@ -2,17 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 5000; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello Chris!");
+  res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>Chris</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
