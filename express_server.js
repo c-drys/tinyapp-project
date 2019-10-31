aa9b3405d9a249743b70975822af35797300f7c7
drystar <chris.drysdale12@gmail.com>
2019-10-31 22:50:14 +0000

    clarify register/ login pages

--- express_server.js
+++ express_server.js
@@ -8,21 +8,21 @@ app.set("view engine", "ejs");
 
 // convert the request body from a buffer to a readable string
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
-  "akjbjb": "http://shop.lululemon.com"
+  akjbjb: "http://shop.lululemon.com"
 };
 
 const users = {
-  "userRandomID": {
+  userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
     password: "password101"
   },
-  "user2RandomID": {
+  user2RandomID: {
     id: "user2RandomID",
     email: "user2@example.com",
     password: "password202"
@@ -30,21 +30,22 @@ const users = {
 };
 
 // // implement to always return 6 characters
-function generateRandomString() {
-  let str = '';
-  let alphanum = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const generateRandomString = function() {
+  let str = "";
+  let alphanum = "abcdefghijklmnopqrstuvwxyz0123456789";
   let alphanumlength = alphanum.length;
   for (let i = 0; i < 6; i++) {
     str += alphanum.charAt(Math.floor(Math.random() * alphanumlength));
   }
+  console.log("string", str);
   return str;
-}
+};
 
 // scan object for user ID
 const findID = function(users, email) {
   for (const newUserID in users) {
-    console.log(users[newUserID].email);
-    console.log(email);
+    // console.log(users[newUserID].email);
+    // console.log(email);
     if (users[newUserID].email === email) {
       return users[newUserID];
     }
@@ -62,33 +63,33 @@ app.get("/urls", (req, res) => {
 });
 
 // add GET register_ejs template route
-app.get('/register', (req, res) => {
+app.get("/register", (req, res) => {
   let templateVars = {
-    user: users[req.cookies["user_id"]],
+    user: users[req.cookies["user_id"]]
   };
-  res.render('urls_registration', templateVars);
+  res.render("urls_register", templateVars);
 });
 
 // add GET access to json
 app.get("/urls.json", (req, res) => {
   let templateVars = {
-    user: users[req.cookies["user_id"]],
+    user: users[req.cookies["user_id"]]
   };
   res.json(urlDatabase, templateVars);
 });
 
 // add GET for urls_login
-app.get('/login', (req, res) => {
+app.get("/login", (req, res) => {
   let templateVars = {
-    user: users[req.cookies["user_id"]],
+    user: users[req.cookies["user_id"]]
   };
-  res.render('urls_login', templateVars);
+  res.render("login", templateVars);
 });
 
 // add GET index_new template route
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    user: users[req.cookies["user_id"]],
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_new", templateVars);
 });
@@ -99,52 +100,59 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-app.get('/urls/:shortURL', (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     user: users[req.cookies["user_id"]],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
-  res.render('urls_show', templateVars);
+  res.render("urls_show", templateVars);
 });
 
-app.get('/u/:shortURL', (req, res) => {
+app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
 // add POST delete route re-direct
-app.post('/urls/:shortURL/delete', (req, res) => {
+app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
-  res.redirect('/urls');
+  res.redirect("/urls");
 });
 
-app.post('/urls/:shortURL', (req, res) => {
+app.post("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   let longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
-  res.redirect('/urls');
+  res.redirect("/urls");
 });
 
 // add POST username for username cookie
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const user = findID(users, req.body.email);
-  console.log("users ID'd", user);
-  res.cookie("user_id", user.id);
-  // console.log("username", req.body.username)
-  res.redirect('/urls');
+  if  (req.body.email === "" || req.body.password === "") {
+    res.sendStatus(400);
+  } else if (!user) {
+    res.sendStatus(403);
+  } else {
+    console.log("users ID'd", user);
+
+    res.cookie("user_id", user.id);
+    // console.log("username", req.body.username)
+    res.redirect("/urls");
+  }
 });
 
-// add POST register for new user 
-app.post('/register', (req, res) => {
-  const newUserID = generateRandomString();
+// add POST register for new user
+app.post("/register", (req, res) => {
+  let newUserID = generateRandomString();
   if (req.body.email === "" || req.body.password === "") {
     res.sendStatus(400);
   } else {
     users[newUserID] = {
-      "id": req.body.newUserID,
-      "email": req.body.email,
-      "password": req.body.password
+      id: newUserID,
+      email: req.body.email,
+      password: req.body.password
     };
   }
   console.log(users);
@@ -153,7 +161,6 @@ app.post('/register', (req, res) => {
   // inspect data object contents
   res.redirect(`/urls`);
 });
-
 
 // // AlreadyRegistered
 // for(let key in users) {
@@ -167,25 +174,22 @@ app.post('/register', (req, res) => {
 // }
 
 // add POST logout & clear user cookie
-app.post('/logout', (req, res) => {
-  console.log(req.body);
+app.post("/logout", (req, res) => {
+  //console.log(req.body);
   res.clearCookie("user_id", users[req.cookies["user_id"]]);
-  res.redirect('/urls');
+  res.redirect("/urls");
 });
-
 
 // // add GET register template route
 // app.get("/register", (req, res) => {
 //   res.render("register");
 // });
 
-
 // //add POST edit route redirect ??
 // app.post("/urls/:shortURL/edit", (req, res) => {
 //   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
 //   res.render("urls_show", templateVars);
 // });
-
 
 // catach all [failsafe]
 // app.get('/' (req, res)) => {
@@ -200,11 +204,11 @@ app.listen(PORT, () => {
 //   console.log(req.body);  // Log the POST request body to the console
 //   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 // });
-  
+
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
 // });
-  
+
 // app.get("/hello", (req, res) => {
 //   res.send("<html><body>Hello <b>Chris</b></body></html>\n");
 // });
