de2e51cac5b8958bd7b0f93ae19a0b404dc013de
drystar <chris.drysdale12@gmail.com>
2019-10-31 05:16:19 +0000

    create a users object

--- express_server.js
+++ express_server.js
@@ -83,6 +83,18 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
