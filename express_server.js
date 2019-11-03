85505771275e0d419266f4fc23801fdb16559cbb
drystar <chris.drysdale12@gmail.com>
2019-11-03 04:58:55 +0000

    clean up

--- express_server.js
+++ express_server.js
@@ -1,35 +1,17 @@
 const cookieSession = require('cookie-session')
 const express = require("express");
 const app = express();
-//const cookieParser = require("cookie-parser");
 const bcrypt = require('bcrypt');
+const bodyParser = require("body-parser");
 const PORT = 5000; // default port 8080
-
 const { generateRandomString, getUserByEmail, urlsForUser } = require('./helpers.js');
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 app.use(cookieSession({
   name: 'session',
   keys: [/* secret keys */
   'asdfghjkl']
 }))
-//app.use(cookieParser());
-app.set("view engine", "ejs");
-
-// convert the request body from a buffer to a readable string
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const urlDatabase = {
-
-  'b2xVn2': { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
-  '9sm5xK': { longURL: "http://www.google.ca", userID: "user2RandomID" },
-  'akjbjb': { longURL: "http://shop.lululemon.com", userID: "userRandomID" },
-  'aq684i': { longURL: "http://bbc.co.uk/iplayer", userID: "user2RandomID" },
-  'yimgsv': { longURL: "http://vancouver.ca", userID: "user2RandomID" },
-  'f20flh': { longURL: "http://www.instagram.com", userID: "userRandomID" },
-  'kkzlwh': { longURL: "http://www.urbandictionary.com", userID: "user2RandomID" },
-  'tagwgw': { longURL: "http://www.gq.com", userID: "user2RandomID" }
-};
 
 const users = {
   userRandomID: {
@@ -46,6 +28,44 @@ const users = {
     }
 };
 
+const urlDatabase = {
+
+  'b2xVn2': { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
+  '9sm5xK': { longURL: "http://www.google.ca", userID: "user2RandomID" },
+  'akjbjb': { longURL: "http://shop.lululemon.com", userID: "userRandomID" },
+  'aq684i': { longURL: "http://bbc.co.uk/iplayer", userID: "user2RandomID" },
+  'yimgsv': { longURL: "http://vancouver.ca", userID: "user2RandomID" },
+  'f20flh': { longURL: "http://www.instagram.com", userID: "userRandomID" },
+  'kkzlwh': { longURL: "http://www.urbandictionary.com", userID: "user2RandomID" },
+  'tagwgw': { longURL: "http://www.gq.com", userID: "user2RandomID" }
+};
+
+
+//get routes
+
+// add GET access to json
+app.get("/urls.json", (req, res) => {
+  let templateVars = {
+    user: users[req.session.user_id]
+  };
+  res.json(urlDatabase, templateVars);
+});
+
+// add GET register_ejs template route
+app.get("/register", (req, res) => {
+  let templateVars = {
+    user: users[req.session.user_id]};
+  res.render(`urls_register`, templateVars);
+});
+
+// add GET for urls_login
+app.get("/login", (req, res) => {
+  let templateVars = {
+    user: users[req.session.user_id]
+  };
+  res.render(`login`, templateVars);
+});
+
 // add GET index_ejs template route
 app.get('/urls', (req, res) => {
   const currentUser = req.session.user_id;
@@ -57,50 +77,6 @@ app.get('/urls', (req, res) => {
   res.render(`urls_index`, templateVars);
 });
 
-// add GET register_ejs template route
-app.get("/register", (req, res) => {
-  let templateVars = {
-    user: users[req.session.user_id]
-  };
-  res.render(`urls_register`, templateVars);
-});
-
-// add GET access to json
-app.get("/urls.json", (req, res) => {
-  let templateVars = {
-    user: users[req.session.user_id]
-  };
-  res.json(urlDatabase, templateVars);
-});
-
-// add GET for urls_login
-app.get("/login", (req, res) => {
-  let templateVars = {
-    user: users[req.session.user_id]
-  };
-  res.render(`login`, templateVars);
-});
-
-// verify user on registry, redirect if unverified
-const registeredUser = function(email) {
-  for (let user of Object.keys(users)) {
-    if (email === users[user]["email"]) {
-      return registeredUser;
-    }
-  }
-};
-
-// scan object for user ID
-// const getUserByEmail = function(users, email) {
-//   for (const newUserID in users) {
-//     // console.log(users[newUserID].email);
-//     // console.log(email);
-//     if (users[newUserID].email === email) {
-//       return users[newUserID];
-//     }
-//   }
-//   return false;
-// };
 
 // add GET index_new template route
 app.get("/urls/new", (req, res) => {
@@ -114,94 +90,26 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_new", templateVars);
   })
 
-
-  // // res.render("urls_new", templateVars);
-
-  // if (req.session.user_id) {
-//  else {
-//     res.redirect(`/login`);
-//   }
-// });
-
-app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  const user = req.session.user_id
-  const newURL = {};
-  newURL["longURL"] = req.body.longURL;
-  newURL["userID"] = user;
-  urlDatabase[shortURL] = newURL;
-  //console.log(urlDatabase);
-  res.redirect(`/urls`);
-});
-
 app.get("/urls/:shortURL", (req, res) => {
-  const currentUser = req.session.user_id;
-  if (!currentUser) {
-    return res.status(403).send("NO ACCESS Forbidden to Editing this URL");
-  } else {
-  let templateVars = {
-    user: users[req.session.user_id],
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL].longURL
-  };
-  res.render(`urls_show`, templateVars);
-};
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL].longURL;
-  res.redirect(longURL);
-});
-
-// add POST delete route re-direct
-app.post("/urls/:shortURL/delete", (req, res) => {
-  const currentUser = req.session.user_id;
+    const currentUser = req.session.user_id;
     if (!currentUser) {
-      res.redirect(`/access`);
+      return res.status(403).send("NO ACCESS Forbidden to Editing this URL");
     } else {
-    delete urlDatabase[req.params.shortURL];
-      return res.redirect("/urls");
-  }
-});
+    let templateVars = {
+      user: users[req.session.user_id],
+      shortURL: req.params.shortURL,
+      longURL: urlDatabase[req.params.shortURL].longURL
+    };
+    res.render(`urls_show`, templateVars);
+  };
+  });
 
-// add POST edit route re-direct
-app.post("/urls/:shortURL", (req, res) => {
-  const currentUser = req.session.user_id;
-console.log('iii', currentUser)
+  app.get("/u/:shortURL", (req, res) => {
+    const longURL = urlDatabase[req.params.shortURL].longURL;
+    res.redirect(longURL);
+  });
 
-  if (!currentUser) {
-    return res.status(403).send("NO ACCESS Forbidden from Editing this URL");
-  } else {
-    urlDatabase[req.params.shortURL].longURL = req.body.longURL
-    return res.redirect(`/urls`); 
-    }
-});
-
-// const longURL = urlDatabase[req.params.shortURL].longURL;
-//   res.redirect(longURL);
-
-// add POST username for username cookie
-app.post("/login", (req, res) => {
-  const user = getUserByEmail(req.body.email, users);
-if (req.body.email !== user.email) {
-  return res.sendStatus(403)
-}
-if (!bcrypt.compareSync(req.body.password, user.password)) {
-  return res.sendStatus(403)
-}
-
-
-  // // if  (req.body.email === "" || req.body.password === "") {
-  // //   res.sendStatus(400);
-  // } else if (!user) {
-  //   res.sendStatus(403);
-  // } else {
-  //   console.log("users ID'd", user);
-    req.session.user_id = user.id //("user_id", user.id);
-  //   // console.log("username", req.body.username)
-    res.redirect(`/urls`);
-  // // }
-});
+//post routes 
 
 // add POST register for new user
 app.post("/register", (req, res) => {
@@ -218,50 +126,61 @@ app.post("/register", (req, res) => {
     };
     console.log(bcrypt.compareSync('asdf', hashedPassword))
     }
-  // console.log(users);
   req.session.user_id = newUserID;
-  // console.log(newUserID);
-  // inspect data object contents
   res.redirect(`/urls`);
+});
+
+// add POST username for username cookie
+app.post("/login", (req, res) => {
+  const user = getUserByEmail(req.body.email, users);
+if (req.body.email !== user.email) {
+  return res.sendStatus(403)
+} if (!bcrypt.compareSync(req.body.password, user.password)) {
+  return res.sendStatus(403)
+} req.session.user_id = user.id 
+  res.redirect(`/urls`);
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  const user = req.session.user_id
+  const newURL = {};
+  newURL["longURL"] = req.body.longURL;
+  newURL["userID"] = user;
+  urlDatabase[shortURL] = newURL;
+  res.redirect(`/urls`);
+});
+
+
+// add POST delete route re-direct
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const currentUser = req.session.user_id;
+    if (!currentUser) {
+      res.redirect(`/access`);
+    } else {
+    delete urlDatabase[req.params.shortURL];
+      return res.redirect("/urls");
+  }
+});
+
+// add POST edit route re-direct
+app.post("/urls/:shortURL", (req, res) => {
+  const currentUser = req.session.user_id;
+console.log('iii', currentUser)
+  if (!currentUser) {
+    return res.status(403).send("NO ACCESS Forbidden from Editing this URL");
+  } else {
+    urlDatabase[req.params.shortURL].longURL = req.body.longURL
+    return res.redirect(`/urls`); 
+    }
 });
 
 // add POST logout & clear user cookie
 app.post("/logout", (req, res) => {
-  //console.log(req.body);
   req.session = null 
-  // res.clearCookie("user_id", users[req.session.user_id]);
   res.redirect(`/login`);
 });
-
-// // add GET register template route
-// app.get("/register", (req, res) => {
-//   res.render("register");
-// });
-
-// //add POST edit route redirect ??
-// app.post("/urls/:shortURL/edit", (req, res) => {
-//   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-//   res.render("urls_show", templateVars);
-// });
-
-// catach all [failsafe]
-// app.get('/' (req, res)) => {
-// console.log('*', (req, res)
-// res.redirect())
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-// app.post("/urls", (req, res) => {
-//   console.log(req.body);  // Log the POST request body to the console
-//   res.send("Ok");         // Respond with 'Ok' (we will replace this)
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>Chris</b></body></html>\n");
-// });
